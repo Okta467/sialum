b99a3c577541400cddb06c89f1e76044f0305270
okta467 <oktaalfiansyah@gmail.com>
2025-05-13 11:21:35 +0700

    Update css and script declarations for role kepala sekolah

--- kepala_sekolah/_partials/script.php
+++ kepala_sekolah/_partials/script.php
@@ -1,14 +1,7 @@
-<?= $go = $_GET['go'] ?? null ?>
-
 <script src="<?= base_url('assets/js/bootstrap/bootstrap.bundle.min.js') ?>" crossorigin="anonymous"></script>
 <script src="<?= base_url('assets/js/scripts.js') ?>"></script>
 <script src="<?= base_url('assets/js/datatables/datatables-simple.min.js') ?>" crossorigin="anonymous"></script>
-
-<?php if ($go !== 'pengumuman'): ?>
-    
 <script src="<?= base_url('assets/js/datatables/datatables-simple-demo.js') ?>"></script>
-
-<?php endif ?>
 
 <script src="<?= base_url("vendors/sweetalert2/dist/sweetalert2.all.min.js") ?>"></script>
 
@@ -19,5 +12,11 @@
 <script src="<?= base_url('vendors/dropify/js/dropify.min.js') ?>"></script>
 
 <script src="<?= base_url('vendors/moment/moment.min.js') ?>"></script>
+
+<script src="<?= base_url('vendors/easymde/js/easymde.min.js') ?>"></script>
+
+<script src="<?= base_url('vendors/marked/marked.min.js') ?>"></script>
+
+<script src="<?= base_url('vendors/DOMPurify-3.1.5/dist/purify.min.js') ?>"></script>
 
 <script src="<?= base_url('assets/js/MY_scripts.js') ?>"></script>
